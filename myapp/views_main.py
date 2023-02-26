dda282c090bec05b086553fc10d792cbc2f39ea7
sc19jwh <sc19jwh@leeds.ac.uk>
2023-02-26 09:41:10 +0000

    Closes #8; Add and delete trips and destinations.

--- myapp/views_main.py
+++ myapp/views_main.py
@@ -96,6 +96,17 @@ def mytrips(request, username):
         return redirect('signin')
     if User.objects.get(username=username) != request.user:
         raise Http404
+    if request.method == 'POST':
+        # If POST adding trip
+        if 'add_trip_form' in request.POST:
+            trip = Trip.objects.create(
+                user = request.user,
+                title = request.POST.get('title')
+            )
+        # If POST deleting trip
+        elif 'delete_trip_form' in request.POST:
+            trip = get_object_or_404(Trip, id=request.POST.get('delete_trip_form'))
+            trip.delete()
     trips = Trip.objects.filter(user=request.user)
     for trip in trips:
         trip.start_date = trip.destination_set.aggregate(Min('start_date'))['start_date__min']
